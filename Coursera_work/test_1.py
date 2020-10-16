4f5db17d396547b04ceb8e0b38c784ddb55c0680
VladG <agerman@yandex.ru>
2020-10-17 01:33:08 +1000

    Commit Home

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,6 +1,8 @@
 A = int(input())
 B = int(input())
-if A > B:
-    print(A)
+C = int(input())
+D = int(input())
+if A - 1 <= C <= A + 1 and B - 1 <= D <= B + 1:
+    print("Yes")
 else:
-    print(B)
+    print("No")
