a179299a54dd817d3af04bc4358731d72d20e4b0
Andger1975 <agerman@yandex.ru>
2020-10-24 20:23:50 +1000

    Commit Office

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,8 +1,12 @@
-n = int(input())
-ost = n % 10
-if 10 <= n <= 20 or ost >= 5 or ost == 0:
-    print(n, 'korov')
-elif n % 10 == 1:
-    print(n, 'korova')
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if x1 > 8 or y1 > 8 or x2 > 8 or y2 > 8 \
+        or x1 < 0 or y1 < 0 or x2 < 0 or y2 < 0:
+    print('NO')
 else:
-    print(n, 'korovy')
+    if (x1 + x2) % 2 == (y1 + y2) % 2 and y2 > y1:
+        print('YES')
+    else:
+        print("NO")
