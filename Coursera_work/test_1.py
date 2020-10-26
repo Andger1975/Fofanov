6d82f8b206ba91f9280901276b3c1b7003308589
Andger1975 <agerman@yandex.ru>
2020-10-26 17:47:07 +1000

    Commit Office

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,12 +1,9 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
-if x1 > 8 or y1 > 8 or x2 > 8 or y2 > 8 \
-        or x1 < 0 or y1 < 0 or x2 < 0 or y2 < 0:
+a = int(input())
+b = int(input())
+c = int(input())
+if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
+    print('NO')
+elif not a % 2 == 0 and not b % 2 == 0 and not c % 2 == 0:
     print('NO')
 else:
-    if (x1 + x2) % 2 == (y1 + y2) % 2 and y2 > y1:
-        print('YES')
-    else:
-        print("NO")
+    print('YES')
