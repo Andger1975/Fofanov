50b0f57e608b336ebf0e99e3329f553d883e7534
Andger1975 <agerman@yandex.ru>
2020-10-28 17:22:36 +1000

    Commit Office

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,9 +1,25 @@
-a = int(input())
-b = int(input())
-c = int(input())
-if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
-    print('NO')
-elif not a % 2 == 0 and not b % 2 == 0 and not c % 2 == 0:
-    print('NO')
+a1, b1, c1 = int(input()), int(input()), int(input())
+a2, b2, c2 = int(input()), int(input()), int(input())
+if b1 > c1:
+    (b1, c1) = (c1, b1)
+if a1 > c1:
+    (a1, c1) = (c1, a1)
+if a1 > b1:
+    (a1, b1) = (b1, a1)
+if b2 > c2:
+    (b2, c2) = (c2, b2)
+if a2 > c2:
+    (a2, c2) = (c2, a2)
+if a2 > b2:
+    (a2, b2) = (b2, a2)
+if (a1 * b1 * c1) == (a2 * b2 * c2) \
+        and a1 == a2 and b1 == b2 and c1 == c2:
+    print("Boxes are equal")
+elif (a1 * b1 * c1) < (a2 * b2 * c2) \
+        and a1 <= a2 and b1 <= b2 and c1 <= c2:
+    print("The first box is smaller than the second one")
+elif (a1 * b1 * c1) > (a2 * b2 * c2) \
+        and a1 >= a2 and b1 >= b2 and c1 >= c2:
+    print("The first box is larger than the second one")
 else:
-    print('YES')
+    print("Boxes are incomparable")
