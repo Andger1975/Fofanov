c7f04d4af6b180ee68777bf4b7e57a67052e24ac
Andger1975 <agerman@yandex.ru>
2020-11-02 18:37:51 +1000

    Commit Office

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,6 +1,8 @@
-k = int(input())
-var1 = 3
-if k < var1 or k == 4 or k == 7:
-    print('NO')
+k, m, n = int(input()), int(input()), int(input())
+if n <= k:
+    minT = 2 * m
+elif n * 2 % k == 0:
+    minT = m * (n * 2 // k)
 else:
-    print('YES')
+    minT = m * (1 + (n * 2 // k))
+print(minT)
