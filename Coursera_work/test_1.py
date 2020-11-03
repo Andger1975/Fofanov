e6161277e2b42ccc0005247896cf4b4054cb6bd9
Andger1975 <agerman@yandex.ru>
2020-11-03 22:23:36 +1000

    Commit Office

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,8 +1,8 @@
-k, m, n = int(input()), int(input()), int(input())
-if n <= k:
-    minT = 2 * m
-elif n * 2 % k == 0:
-    minT = m * (n * 2 // k)
-else:
-    minT = m * (1 + (n * 2 // k))
-print(minT)
+now = int(input())
+# maxNum = 1
+
+while i  now:
+    print(i)
+    maxNum = maxNum + 1
+    i = maxNum ** 2
+print()
