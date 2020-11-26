fc1f605982d4f488aa8bfc28a7930533b5bf195c
Andger1975 <agerman@yandex.ru>
2020-11-26 18:55:33 +1000

    Office

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,7 +1,11 @@
-element = -1
+xElement = -1
+digital = xElement
 i = 0
-while element != 0:
-    if element % 2 == 0:
-        i += 1
-    element = int(input())
+while xElement != 0:
+    xElement = int(input())
+    if digital < xElement:
+        i = digital
+        digital = xElement
+    elif xElement != 0 and i < xElement:
+        i = xElement
 print(i)
