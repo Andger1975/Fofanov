d1df43e98c147ac203f33ed778144d9d0b4cf40d
Andger1975 <agerman@yandex.ru>
2020-11-29 15:58:30 +1000

    Office

--- Coursera_work/test_1.py
+++ Coursera_work/test_1.py
@@ -1,11 +1,8 @@
-xElement = -1
-digital = xElement
-i = 0
-while xElement != 0:
-    xElement = int(input())
-    if digital < xElement:
-        i = digital
-        digital = xElement
-    elif xElement != 0 and i < xElement:
-        i = xElement
-print(i)
+N1 = int(input())
+N2 = 0
+while N1 > 0:
+    digit = N1 % 10
+    N1 = N1 // 10
+    N2 = N2 * 10
+    N2 = N2 + digit
+print(N2)
